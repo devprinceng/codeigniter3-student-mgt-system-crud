28dd7ac0346c098cec1d8d507e9a82df2cf50613
devprinceng <devprinceng@gmail.com>
2024-04-09 19:14:35 +0100

    added some test student data

--- application/controllers/StudentController.php
+++ application/controllers/StudentController.php
@@ -4,4 +4,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class StudentController extends CI_Controller
 {
 
+    public $students = [
+        [
+            "id" => 1,
+            'name' => 'prince ojoi'
+        ],
+        [
+            "id" => 2,
+            'name' => 'mercy'
+        ],
+        [
+            "id" => 3,
+            'name' => 'melvin'
+        ]
+    ];
+
 }
