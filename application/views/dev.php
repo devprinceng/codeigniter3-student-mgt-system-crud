3350958aaae161d0ccf3e5a3fc9ca70dc04be440
devprinceng <devprinceng@gmail.com>
2024-04-09 19:06:49 +0100

    added project setup, with student controller and model

---
+++ application/views/dev.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') or exit('No Direct Script Access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+</head>
+
+<body>
+    <h1>Hey i'm testing Controllers an views here</h1>
+</body>
+
+</html>
---
+++ application/views/partials/head.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') or exit('Direct script access not allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students</title>
+</head>
+
+<body>
+    <div style="display: flex; justify-content:center; flex-direction:column; text-align:center; padding:30px">
