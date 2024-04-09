a84750ce2567c12d76d0a4222ea58c93994073e9
devprinceng <devprinceng@gmail.com>
2024-04-09 19:18:24 +0100

    added test data to student model

--- application/controllers/StudentController.php
+++ application/controllers/StudentController.php
@@ -3,7 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class StudentController extends CI_Controller
 {
-
     public $students = [
         [
             "id" => 1,
@@ -18,5 +17,23 @@ class StudentController extends CI_Controller
             'name' => 'melvin'
         ]
     ];
-
 }
+
+ //test
+ public function student_data()
+ {
+     $class = $this->load_class();
+     $data = [
+         'name' => 'Prince',
+         'sex' => 'male',
+         'department' => 'computer science',
+         'class' => $class,
+     ];
+
+     return $data;
+ }
+ private function load_class()
+ {
+     return "300L";
+ }
+
--- application/models/StudentModel.php
+++ application/models/StudentModel.php
@@ -2,4 +2,18 @@
 
 class StudentModel extends CI_Model
 {
+    public $students = [
+        [
+            "id" => 1,
+            'name' => 'prince ojoi'
+        ],
+        [
+            "id" => 2,
+            'name' => 'mercy'
+        ],
+        [
+            "id" => 3,
+            'name' => 'melvin'
+        ]
+    ];
 }
